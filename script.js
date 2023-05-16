3fec10bee29f80a83527dc8f8cd00be4f3080b1c
Taliane Moussassa <talianegloire392@gmail.com>
2023-05-16 09:43:04 +0100

    adds countries list

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("Testing, testing, 1 2 3 ...")
+
+let apiData; 
+
+const countriesList = document.querySelector("#countriesList");
+
+async function setup(){
+    fetchRestCountries();
+    countriesList.innerHTML = "";
+    apiData = await fetchRestCountries()
+    apiData.map(country => {
+        createNewCountryElement (country)
+        
+})
+} 
+    const fetchRestCountries = async () => {
+        const response = await fetch ("https://restcountries.com/v3.1/all")
+
+        const data = await response.json();
+
+        return data;
+        
+    }   
+
+    setup();
+
+    function createNewCountryElement (country) {
+        const newListItem = document.createElement("li")
+        newListItem.innerText = `${country.name.common} population: ${country.population}`
+        countriesList.appendChild(newListItem);
+    }
